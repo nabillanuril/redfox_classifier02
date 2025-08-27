acbeaf875ddaa4dc56bfbf5e4129ba9548c8b7ff
nabillanuril <100427459+nabillanuril@users.noreply.github.com>
2025-08-27 01:46:00 +0100

    Update 11.delete files.R

--- 11.delete files.R
+++ 11.delete files.R
@@ -1,13 +1,13 @@
-# Path to the folder
-path <- "C:/path/to/your/folder"
-
 remove <- function(path, full.names = TRUE, recursive = TRUE) {
 # List all files inside (recursive)
 files <- list.files(path, full.names = full.names, recursive = recursive)
-
 # Keep only files (not directories)
 files <- files[!dir.exists(files)]
-
 # Delete them
 file.remove(files)
 }
+
+remove("F:/MSc Ecology & Data Science Research/3. augment data")
+remove("F:/MSc Ecology & Data Science Research/3. augment data_train")
+remove("F:/MSc Ecology & Data Science Research/3. test_data")
+remove("F:/MSc Ecology & Data Science Research/3. train_data")
